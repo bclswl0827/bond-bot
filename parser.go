64ed48a7418a6310adbd043769a8f7929d2267b9
Yuki Kikuchi <24319042+bclswl0827@users.noreply.github.com>
2022-08-08 09:18:23 +0800

    Update parser.go

--- parser.go
+++ parser.go
@@ -42,15 +42,15 @@ func BondFilter(data interface{}) string {
 	for _, v := range bonds.Result.Data {
 		// 匹配今天
 		if v.Date == time.Now().Format("2006-01-02")+" 00:00:00" {
-			message += "  ·  " + v.Name + "（" + v.Code + " / " + v.Rating + "）\n"
+			message += "·" + v.Name + "（" + v.Code + " / " + v.Rating + "）\n"
 		}
 		// 匹配明天
 		if v.Date == time.Now().Add(time.Hour*24).Format("2006-01-02")+" 00:00:00" {
-			message += "  ·  " + v.Name + "（" + v.Code + " / " + v.Rating + " / 预约）\n"
+			message += "·" + v.Name + "（" + v.Code + " / " + v.Rating + " / 预约）\n"
 		}
 		// 匹配后天
 		if v.Date == time.Now().Add(time.Hour*24*2).Format("2006-01-02")+" 00:00:00" {
-			message += "  ·  " + v.Name + "（" + v.Code + " / " + v.Rating + " / 预约）\n"
+			message += "·" + v.Name + "（" + v.Code + " / " + v.Rating + " / 预约）\n"
 		}
 	}
 
